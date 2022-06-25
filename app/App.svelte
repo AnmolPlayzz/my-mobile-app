354f5cbb6b0c0ea611af103af7dd27b142953a83
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2022-06-25 23:01:16 +0530

    MORE SVELTE!!!!!!!!!!

---
+++ app/App.svelte
@@ -0,0 +1,27 @@
+<page>
+    <actionBar title="Svelte Native App" />
+    <gridLayout>
+        <button>
+            idk
+        </button>
+    </gridLayout>
+</page>
+
+<script lang="typescript">
+import { Button } from "@nativescript/core";
+
+    let message: string = "Blank Svelte Native App"
+</script>
+
+<style>
+    button {
+        background-color: cadetblue;
+    }
+
+    .info .fas {
+        color: #3A53FF;
+    }
+    .info {
+        font-size: 20;
+    }
+</style>
